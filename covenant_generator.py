d8f646275cf2e837c6ecabe9f1a397fcd0b72419
roei.yehuda <roei.yehuda@cs.huji.ac.il>
2020-08-25 15:51:42 +0300

    created covenant_generator.py

---
+++ covenant_generator.py
@@ -0,0 +1,21 @@
+# todo - check for any "???" left...
+
+"""
+This file is part of the ??? project which can be found at ???github...???
+This project was a created for a university course, and then made publicly available.
+Introduction to Cryptocurrencies, course #67513, Hebrew University Jerusalem Israel, Aug 2020.
+Avigail Suna; email ???
+Roei Yehuda; email ???
+
+The purpose of the code here is to implement a cashScript (???link) covenants generator for BCH smart contracts.
+"""
+
+import os.path
+import glob
+import datetime
+import sys
+from datetime import datetime
+import re
+import numpy as np
+import pandas as pd
+
