6d9c5098c6f8dc26fd733fe8e5c1f0102898aefb
roei.yehuda <roei.yehuda@cs.huji.ac.il>
2020-08-26 03:43:28 +0300

    added allow_cold

--- covenant_generator.py
+++ covenant_generator.py
@@ -66,11 +66,19 @@ class utils():
 
 class cov_gen():
 
-    def __init__(self, contract_name='cov', cashScript_pragma='0.4.0', intro_comment=''):
-        # todo doc
+    def __init__(self, contract_name='cov', cashScript_pragma='0.4.0', miner_fee=1000, intro_comment=''):
+        """
+        Initializing a cov_gen.
+        :param contract_name: string
+        :param cashScript_pragma: string - cashScript version
+        :param miner_fee: int - an estimation of a reasonable miner_fee, according to the contract's size.
+        :param intro_comment: string - the user might want to add a free comment. It is unlimited in size (as it
+                appears only in the cash file, not in the bytecode) and appears at the very beginning of the file.
+        """
 
         self.contract_name = contract_name
         self.pragma = cashScript_pragma
+        self.miner_fee = miner_fee
         self.intro_comment = intro_comment
 
         # self.constructor_args is a dictionary with arguments names as keys and tuples like
@@ -89,7 +97,7 @@ class cov_gen():
         :param name: string
         :param description: (optional) string - this is used for inner purposes, and will not be written to the output contract
         """
-        # we record the order of arrival simply with len(self.constructor_args.keys())
+        # we record the order of arrival with len(self.constructor_args.keys())
         self.constructor_args[name] = (type, name, len(self.constructor_args.keys()), description)
 
     def _get_constructor_text(self):
@@ -97,14 +105,9 @@ class cov_gen():
         calculate the final string for the constructor
         :return: string
         """
-
         sorted_args_tuples = list(self.constructor_args.values())
         sorted_args_tuples.sort(key=lambda t: t[2])     # '2' since the index of the constructor_arg is the third argument in each tuple
-
-        text = ''
-        for arg_tup in sorted_args_tuples:
-            text += arg_tup[0] + ' ' + arg_tup[1] + ', '
-        return text[:-2]
+        return ', '.join([sorted_args_tuples[i][0] + ' ' + sorted_args_tuples[i][1] for i in range(len(sorted_args_tuples))])
 
     def _add_to_functions(self, fn_name, fn_text, description=''):
         """
@@ -120,12 +123,9 @@ class cov_gen():
         calculate the final string for the contract body
         :return: string
         """
-        text = ''
-        for fn_tup in self.functions.values():
-            # we strip the input fn_text, then add one indentation ('\t') to its entirety and then another indentation
-            # to its inner scope
-            text += utils().indent(utils().indent_inside(fn_tup[0].strip())) + '\n'
-        return text
+        # we strip the input fn_text, then add one indentation ('\t') to its entirety and then another indentation
+        # to its inner scope
+        return '\n\n'.join([utils().indent(utils().indent_inside(fn_tup[0].strip())) for fn_tup in self.functions.values()])
 
     def get_script(self):
         """
@@ -177,48 +177,111 @@ class cov_gen():
         return byte_code
 
     def basic_covenant(self,
-                       number_of_receipients=1):
-                       #
-                       # allowed_output_pkh=[], use_all_pkh=False,
-                       # allowed_output_sh=[], use_all_sh=False):
+                       n_recipients=1,
+                       p2sh_recipients_indices=[],
+                       include_any=True):
 
-        # todo - maybe we need to check input is valid
-
-        for i in range(number_of_receipients):
-            self._add_to_constructor(type="bytes20",
-                                     name="recepient_{}".format(i),
-                                     description="allowed_recepient")
+        # todo - maybe we need to check that the input is valid
 
         fn_name = 'spend'
 
+        p2pkh_recipients_indices = [i for i in list(range(n_recipients)) if i not in p2sh_recipients_indices]
+
+        for i in p2pkh_recipients_indices:
+            self._add_to_constructor(type="bytes20",
+                                     name="recepient_{}_pkh".format(i),
+                                     description="allowed_recepient")
+
+        """ OLDER VERSION (before transitioning to fn_lines we used a more explicit fn_text)
+
         fn_text =   "function " + fn_name + "(pubkey pk, sig s) {\n" \
+                    "require(hash160(pk) == pkh);\n" \  # todo
                     "require(checkSig(s, pk));\n" \
                     "// Create and enforce outputs\n" \
-                    "int minerFee = 1000; // hardcoded fee\n" \
+                    "int minerFee = " + str(self.miner_fee) + "; // hardcoded fee\n" \
                     "bytes8 amount = bytes8(int(bytes(tx.value)) - minerFee);\n" \
                     ""
 
-        for i in range(number_of_receipients):
-            fn_text += "bytes34 out_{} = new OutputP2PKH(amount, recepient_{});\n".format(i,i)
+        for i in range(n_recipients):
+            fn_text += "bytes34 out_{} = new OutputP2PKH(amount, recepient_{}_pkh);\n".format(i,i)
 
 
         fn_text += "require("
-        for i in range(number_of_receipients):
+        for i in range(n_recipients):
             fn_text += "tx.hashOutputs == hash256(out_{}) || ".format(i)
         fn_text = fn_text[:-4] + ");\n"
 
         fn_text += "}"
 
         self._add_to_functions(fn_name, fn_text, description='basic_covenant')
+        
+        """
+
+        fn_lines = []
+        fn_lines.append("function " + fn_name + "(pubkey pk, sig s) {")
+
+        # # this is commented out because it is not necessary to check *who* the signer is. But useful so don't delete yet :)
+        # req_pkh_cond = ' || '.join(["hash160(pk) == recepient_{}_pkh".format(i) for i in p2pkh_recipients_indices])
+        # fn_lines.append("require(" + req_pkh_cond + ");")
+
+        fn_lines.append("require(checkSig(s, pk));")
+        fn_lines.append("// Create and enforce outputs")
+        fn_lines.append("int minerFee = " + str(self.miner_fee) + "; // hardcoded fee")
+        amount = "int(bytes(tx.value)) - minerFee" if include_any else "int((int(bytes(tx.value)) - minerFee) / {})".format(n_recipients)
+        fn_lines.append("bytes8 amount = bytes8(" + amount + ");")
+
+        for i in range(n_recipients):
+            if i in p2pkh_recipients_indices:
+                fn_lines.append("bytes34 out_{} = new OutputP2PKH(amount, recepient_{}_pkh);".format(i,i))
+            else:   # this means i is in p2sh_recipients_indices
+                fn_lines.append("bytes32 out_{} = new OutputP2SH(amount, recepient_{}_pkh);".format(i,i))   # todo
+
+        if include_any:
+            hashOutputs_cond = ' || '.join(["tx.hashOutputs == hash256(out_{})".format(i) for i in range(n_recipients)])
+        else:
+            hashOutputs_cond = 'tx.hashOutputs == hash256(' + ' + '.join(['out_{}'.format(i) for i in range(n_recipients)]) + ')'
+        fn_lines.append("require(" + hashOutputs_cond + ");")
+
+        fn_lines.append('}')
+
+        fn_text = '\n'.join(fn_lines)
+        self._add_to_functions(fn_name, fn_text, description='basic_covenant')
+
+
+    def allow_cold(self, n=1):
+        """
+        This method basically allows easy and unlimited access to the funds in the contract for n people defined
+        in the constructor.
+        :param n: int
+        """
+
+        fn_name = 'cold'
+
+        for i in range(n):
+            self._add_to_constructor(type="bytes20",
+                                     name="cold_{}_pkh".format(i),
+                                     description="cold_{}".format(i))
+
+        fn_lines = []
+        fn_lines.append("function " + fn_name + "(pubkey pk, sig s) {")
+        req_pkh_cond = ' || '.join(["hash160(pk) == cold_{}_pkh".format(i) for i in range(n)])
+        fn_lines.append("require(" + req_pkh_cond + ");")
+        fn_lines.append("require(checkSig(s, pk));")
+        fn_lines.append('}')
+
+        fn_text = '\n'.join(fn_lines)
+        self._add_to_functions(fn_name, fn_text, description='cold')
+
 
 
 # if __name__ == 'main':
 
 cg = cov_gen()
-cg.basic_covenant(number_of_receipients=2)
+cg.basic_covenant(n_recipients=2, include_any=True)
+cg.allow_cold(2)
 print(cg.get_script())
 print()
-print(cg.compile_script())
+# print(cg.compile_script())
 
 
 
