0a683e28bb1284e6946fb081cee61e675b3e3a81
avigail_suna <avigail.suna@mail.huji.ac.il>
2020-08-28 11:18:30 +0300

    break for travel

--- cli.py
+++ cli.py
@@ -1,7 +1,8 @@
 # this is the command line interface for our covenant generator
-from covenant_generator import *
-
+from .covenant_generator import cov_gen, cov_fn
 from termcolor import colored
+import copy
+
 
 # import click
 
@@ -38,13 +39,13 @@ from termcolor import colored
 #     create_cov_gen()
 
 
-
 class cov_gen_CLI():
 
     def __init__(self):
 
         # creating covenant
-        self.cov_init_args = copy.deepcopy(cov_gen.cov_init_kwargs_d)   # {'contract_name':'cov', 'cashScript_pragma':'0.4.0', 'miner_fee':1000, 'intro_comment':''}
+        self.cov_init_args = copy.deepcopy(
+            cov_gen.cov_init_kwargs_d)  # {'contract_name':'cov', 'cashScript_pragma':'0.4.0', 'miner_fee':1000, 'intro_comment':''}
         self.funcs_list = []
 
         # global commands - these are commands that the user may input at any point
@@ -52,11 +53,23 @@ class cov_gen_CLI():
 
         self.run()
 
+    def print_comment(self, comment: str):
+        print(colored(comment, 'yellow'))
+
+    def print_error(self, err: str):
+        print(colored(err, 'red'))
 
     def run(self):
         # print intro:
-        intro = "intro msg"
-        print(colored(intro, 'yellow'))
+        # Rs = ['-o- operators', '-r- recipients', '-a- amount', '-t- time']
+        options = ['operators - could key participants ',
+                   'recipients - hot key participants',
+                   'amount - the max amount that can be drawn at any transaction',
+                   'time - waiting period in which the transaction can be evoked']
+        intro = 'Hello, \n' \
+                'what restrictions would you like to put on your money in BCH? \n' \
+                'your options are: \n\n{}\n\nlets start!\n'.format('\n'.join(options))
+        self.print_comment(intro)
         # set init
         # in_between_fns
 
@@ -66,58 +79,142 @@ class cov_gen_CLI():
     def set_init(self):
         pass
 
-    def parse_input(self, name='', tp=str, default=None, i_msg='Sorry, no more information is provided. You can always try -h'):
+    def parse_input(self, name='', tp=str, default=None, choices=None,
+                    i_msg='Sorry, no more information is provided. You can always try -h'):
 
         print('enter {}{}: '.format(name, '' if default is None else ' [{}]'.format(str(default))))
         my_input = input()
 
+        # help and global functions
         if my_input in self.globals:
             if my_input == '-i':
-                print(i_msg)
+                self.print_comment(i_msg)
+                my_input = input()
             # if ... # todo
 
-        if default is not None and my_input == '':
-            return default
+        # no input entry
+        if my_input == '':
+            if default is not None:
+                return default
+            else:
+                self.parse_input(name, tp, default)
 
+        # wrong input
         try:
-            return tp(my_input)
+            my_input_in_tp = tp(my_input)
+            if choices is not None:
+                if my_input_in_tp in choices:
+                    return my_input_in_tp
+                else:
+                    self.print_error('Oops! wrong input, your options are {}:'.format(str(choices)))
+                    self.parse_input(name, tp, default)
+            return my_input_in_tp
         except ValueError:
-            print(colored('Oops! {} is not of type {}, try again:'.format(str(my_input), str(tp)), 'red'))
+            self.print_error('Oops! {} is not of type {}, try again:'.format(str(my_input), str(tp)))
             self.parse_input(name, tp, default)
 
     def _h(self):
+        """ help information global """
         pass
 
     def _exit(self):
+        """ exit the program """
         pass
 
     def _clear(self):
+        """ restart, clear all """
         pass
 
-    def add_fn(self):
+    def fn_rs_operators(self):
+        """ restrict operators """
+        r_d = copy.deepcopy(cov_fn.restrict_operators_kwargs_d)
+        r_d['n'] = self.parse_input(name='number of desired operators', tp=int, default=1,
+                                    i_msg='type the number of desired operators and then click enter:')
+        return r_d
 
-        fn_name = ''
+    def fn_rs_recipients(self):
+        """ restrict recipients """
+        r_d = copy.deepcopy(cov_fn.restrict_recipients_kwargs_d)
+        r_d['n_PKH'] = self.parse_input(name='number of desired recipients (P2PKH)', tp=int, default=1,
+                                        i_msg='type the number of desired recipients and then click enter:')
+
+        r_d['n_SH'] = self.parse_input(name='number of desired contract recipients (P2SH)', tp=int, default=0,
+                                       i_msg='type the number of desired contract recipients and then click enter:')
+
+        def get_y_n(comment: str):
+            self.print_comment(comment)
+            return self.parse_input(name='y or n', tp=str, default=None, choices=['y', 'n'],
+                                    i_msg='{}}? type y or n and then enter)'.format(comment))
+
+        r_d['require_recipient_sig'] = get_y_n('would you like the tx to only be signed by one of the pkh recipients?') == 'y'
+        r_d['include_all'] = get_y_n('would you like the funds to be distributed to all?') == 'y'
+
+        return r_d
+
+    def fn_rs_amount(self):
+        """ restrict the amount that can be pulled in a transaction """
+        r_d = copy.deepcopy(cov_fn.restrict_amount_kwargs_d)
+
+        def limit_amount_y_n(whom:str):
+            self.print_comment('would you like to limit the amount per {}}? (y/n)'.format(whom))
+            return self.parse_input(name='y or n', tp=str, default=None, choices=['y', 'n'],
+                                    i_msg='would you like to limit the amount per {}}? type y or n and then enter)'.format(whom))
+
+        if limit_amount_y_n('transaction') == 'y':
+            r_d['max_amount_per_tx'] = self.parse_input(name='amount limit per transaction', tp=int, default=None,
+                                                        i_msg='pic a transaction limit, must be an integer')
+        if limit_amount_y_n('recipient') == 'y':
+            r_d['max_amount_per_recipient'] = self.parse_input(name='amount limit per recipient', tp=int, default=None,
+                                                               i_msg='pic a limit per recipient, must be an integer')
+        return r_d
+
+    def fn_rs_time(self):
+        """ create a time window for cancellation """
+        r_d = copy.deepcopy(cov_fn.restrict_time_kwargs_d)
+
+        r_d_min_max = self.parse_input(name='min or max time window', tp=str, default=None, choices=['min', 'max'],
+                                       i_msg='pick a time limitation, before a certain time passes (min), or after (max)')
+        r_d[r_d_min_max] = self.parse_input(name='time limitation [number][time stamp]', tp=str, default=None,
+                                            i_msg='pick a time limitation a number and then a time stamp, for example "30 days"')
+
+        r_d_typ_lim = self.parse_input(name='type of time limit', tp=str, default=None, choices=['time', 'age'],
+                                       i_msg='pick a type of time limitation, time - absolute, age - relative')
+        r_d['{}_limit'.format(r_d_typ_lim)] = True
+        return r_d
+
+    def add_fn(self):
+        self.print_comment('pick function name')
+        fn_name = self.parse_input(name='function name', tp=str, default=None,
+                                   i_msg='write a name for your function and then click enter:')
         fn_desc = ''
         fn_restrictions = []
 
         def add_restriction():
-            Rs = ['operators', 'recipients', 'amount', 'time']
-            Rs_args = [copy.deepcopy(cov_fn.restrict_operators_kwargs_d),
-                       copy.deepcopy(cov_fn.restrict_recipients_kwargs_d),
-                       copy.deepcopy(cov_fn.restrict_amount_kwargs_d),
-                       copy.deepcopy(cov_fn.restrict_time_kwargs_d)]
+            Rs = ['-o- operators', '-r- recipients', '-a- amount', '-t- time']
+            restrict_txt = 'which restriction(s) would you like to apply?\n' \
+                           'your options are:\n{}' \
+                           '\ntype the first letter of one option and then click enter'.format('\n'.join(Rs))
+            self.print_comment(restrict_txt)
+            r = self.parse_input(name='restriction', tp=str, default=None, choices=['o', 'r', 'a', 't'],
+                                 i_msg='type the first letter of one option and then click enter:\n{}'.format('\n'.join(Rs)))
+            if r == 'o':
+                r_args_d = self.fn_rs_operators()
+            elif r == 'r':
+                r_args_d = self.fn_rs_recipients()
+            elif r == 'a':
+                r_args_d = self.fn_rs_amount()
+            elif r == 't':
+                r_args_d = self.fn_rs_time()
 
-            r = ''
+            # r = ''
             # r_args_d = Rs_args[?]
 
             # set r, r_args_d   # todo
-
-            fn_restrictions.append((r, r_d))
+            fn_restrictions.append((r, r_args_d))
 
         # add restrictions in a while loop # todo
 
         self.funcs_list.append((fn_name, fn_desc, fn_restrictions))
-
 
     def generate_cov(self):
         pass
@@ -126,19 +223,14 @@ class cov_gen_CLI():
 """
 Avigail's tasks:
 - tx.time vs tx.age
-- how to print IN COLOR in the cmd
 - add an option to remove fn
 """
 
 
-
-
-
-
 def enter_param(name='', tp=str, has_default=True, default=None):
-    print('enter ' + name + (' ['+str(default)+']: ') if has_default else ':')
+    print('enter ' + name + (' [' + str(default) + ']: ') if has_default else ':')
     my_input = input()
-    if has_default and my_input=='':
+    if has_default and my_input == '':
         return default
     try:
         return tp(my_input)
@@ -183,7 +275,6 @@ if __name__ == '__main__':
     # my_main()
 
     cov_gen_CLI()
-
 
 """
 start
